b7e1d1ea368f5d6100b4ebac3b89671a35812db7
roberto <roberto.almanza05@gmail.com>
2022-12-04 15:02:33 -0700

    ggg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,42 +12,46 @@ const PagesIndex = () => {
   const { info, results } = useFetchData({ page: page });
 
   return (
-    <div className={styles.container}>
+    <div>
       <NavBar />
-      <div className={styles.div}>
-        <h2 className={styles.h2}>personajes:{info.count} </h2>
-      </div>
+      <div className={styles.container}>
+        <div className={styles.div}>
+          <h2 className={styles.h2}>personajes:{info.count} </h2>
+        </div>
 
-      <div className={styles.divimg}>
-        {results.map((character) => {
-          const { name, image, id } = character;
-          return (
-            <div>
-              <img src={`${image}`} className={styles.img} />
-              <h1 className={styles.h1}>
-                <Link href={`/character/${id}`}>{name}</Link>
-              </h1>
-            </div>
-          );
-        })}
+        <div className={styles.divimg}>
+          {results.map((character) => {
+            const { name, image, id } = character;
+            return (
+              <div>
+                <img src={`${image}`} className={styles.img} />
+                <h1 className={styles.h1}>
+                  <Link href={`/character/${id}`}>{name}</Link>
+                </h1>
+              </div>
+            );
+          })}
+        </div>
+        <ButtonComponent
+          onClick={() => {
+            setPage(Math.max(page - 1, 1));
+            window.scrollTo(0, 30);
+          }}
+        >
+          atras
+        </ButtonComponent>
+        <div className={styles.div}>
+          <span className={styles.h2}> pagina:{page}</span>
+        </div>
+        <ButtonComponent
+          onClick={() => {
+            setPage(Math.min(page + 1, info.pages));
+            window.scrollTo(0, 30);
+          }}
+        >
+          siguiente
+        </ButtonComponent>
       </div>
-      <ButtonComponent
-        onClick={() => {
-          setPage(Math.max(page - 1, 1));
-        }}
-      >
-        atras
-      </ButtonComponent>
-      <div className={styles.div}>
-        <span className={styles.h2}> pagina:{page}</span>
-      </div>
-      <ButtonComponent
-        onClick={() => {
-          setPage(Math.min(page + 1, info.pages));
-        }}
-      >
-        siguiente
-      </ButtonComponent>
     </div>
   );
 };
