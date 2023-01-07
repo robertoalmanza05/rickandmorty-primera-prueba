1c98e685c0b849b07842b9609b4d89fcf379f50e
roberto <roberto.almanaz05@gmail.com>
2023-01-07 01:18:07 -0700

    creacion de la carpeta public

--- src/pages/character/[id].tsx
+++ src/pages/character/[id].tsx
@@ -1,9 +1,12 @@
+import html2canvas from 'html2canvas';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { resourceLimits } from 'worker_threads';
 import NavBar from '../../components/navBar';
 import useFetchCharacter from '../../hooks/useFetchCharacter';
 import styles from '../../pages/index.module.css';
+import imagenxd from '/img/1.png';
+import Image from 'next/image'
 type Props = {
   name?: string;
   id: number;
@@ -23,6 +26,18 @@ const Post = (props: Props) => {
     
   }, [router.asPath])
   
+  const onclickFuncion= function(evento){
+    html2canvas(document.querySelector("#aaaaa")).then(canvas => {
+ var img= canvas.toDataURL("image/png");
+ 
+ var link= document.createElement('a');
+ 
+ link.download= 'meme.png'
+ link.href= img;
+link.click();
+
+    });
+  }
 
   const [palabra, setPalabra] = useState("");
   
@@ -46,13 +61,36 @@ const Post = (props: Props) => {
 
         <h1 className={styles.h11}>{result.location?.name}</h1>
       </div>
-      <div
+      <div 
         style={{
           display: 'flex',
           justifyContent: 'center'
         }}
       >
-        <img src={`${result.image}`}></img>
+        <img  id="x" src={`${result.image}`}></img>
+      </div>
+      
+      
+      
+      
+      
+      
+      <div>
+    
+      <img
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        width: '300px',
+        height:'300px'
+
+      }} 
+      id= "aaaaa"src ="/img/1.png"/>  
+      
+         
+      </div>
+      <button onClick={onclickFuncion}>xadAADSAD</button>
+      <div>
       </div>
     </>
   );
